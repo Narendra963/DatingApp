c4d64fedd0363de68c50c8228e03c33ca136f996
karthik p <rago.karthik@outlook.com>
2021-07-10 20:43:09 +0530

    End of section 7

--- client2/src/app/app-routing.module.ts
+++ client2/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './member-details/member-details.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,8 +23,11 @@ const routes: Routes = [
       { path: 'messages', component: MessagesComponent }
     ]
   }
-  ,
+  ,{ path: 'errors', component:TestErrorsComponent },
+  { path: 'not-found', component:NotFoundComponent },
+  { path: 'server-error', component:ServerErrorComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' }
+  
 ];
 
 @NgModule({
